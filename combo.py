b7c213ace6588d1b10872e6f1fed953eda19fd63
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2021-04-10 16:35:50 +1200

    Defined the data storage variables and classes.

--- combo.py
+++ combo.py
@@ -4,3 +4,24 @@
 # IMPORTS
 # Import EasyGUI for the graphical interface.
 import easygui
+# Import Pickle for saving and loading combos.
+import pickle
+
+# DATA STORAGE
+# Defining the global variables and data structures used.
+
+
+class Combo(name, items):
+    """This class defines a combo of items.
+
+    Args:
+        name (str): The name of the combo.
+        items (dict): A dictionary containing the names and prices (as float) of individual items in the combo.
+    """
+    def __init__(self, name, items):
+        self.name = name
+        self.items = items
+
+
+# This array contains all the combos as Combo objects (defined above).
+combos = []
